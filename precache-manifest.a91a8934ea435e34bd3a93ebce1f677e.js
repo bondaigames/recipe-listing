2b4d50247773ec6bf1d0ea40e536550a3822ea54
Hoang Huy <huy3041993@gmail.com>
2019-12-15 12:23:53 +1100

    Updates

---
+++ precache-manifest.a91a8934ea435e34bd3a93ebce1f677e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01edff05f3946b7659b6f0525950ddf1",
+    "url": "/recipe-listing/index.html"
+  },
+  {
+    "revision": "0ababfc41aa7bfb3b9c3",
+    "url": "/recipe-listing/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7bdc5926f9e759b2a479",
+    "url": "/recipe-listing/static/css/main.3f24f012.chunk.css"
+  },
+  {
+    "revision": "0ababfc41aa7bfb3b9c3",
+    "url": "/recipe-listing/static/js/2.29915fc9.chunk.js"
+  },
+  {
+    "revision": "7bdc5926f9e759b2a479",
+    "url": "/recipe-listing/static/js/main.b36f3573.chunk.js"
+  },
+  {
+    "revision": "988330792119e2ba0230",
+    "url": "/recipe-listing/static/js/runtime-main.fb9d3bb4.js"
+  }
+]);
